b115e9e9889947cb3b4a89bc017c5296531cb802
Ryan McGregor <tug81211@temple.edu>
2021-03-03 22:44:25 -0500

    Added logo to the main screen to create aesthetic Hello, World versin of the project

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image
+        style={{width: 180, height: 180, resizeMode: 'contain'}}
+        source = {require('./assets/kic.png')}
+      />
       <Text>Keeping It Casual!</Text>
       <StatusBar style="auto" />
     </View>
