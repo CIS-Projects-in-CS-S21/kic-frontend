38ffe321c176af24d5edb806a1439e4f809682b1
Ryan McGregor <tug81211@temple.edu>
2021-03-04 17:32:45 -0500

    Added documentation for Explore, FindHelp, Login, MentalHealthLog pages

--- Explore.js
+++ Explore.js
@@ -1,10 +1,22 @@
+/**
+ * @fileoverview Explore page - allows users to discover friends and search for users.
+ */
+
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import {Button} from "react-native-web";
 
+
+/**
+ * @class Contains function for rendering Explore screen.
+ */
 class Explore extends React.Component {
   render() {
+  /**
+   * Renders Explore screen components.
+   * @returns {Component}
+   */
       return (
         <View style={styles.container}>
           <Image
@@ -24,6 +36,9 @@ class Explore extends React.Component {
   }
 }
 
+/**
+ * @constant styles creates stylesheet for Explore screen components
+ */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
