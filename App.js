2cb5ac6b1b3055c27d366aabbaf1d2e4fd2017db
Aszliah Delicana <aszliahdelicana@gmail.com>
2021-03-04 16:37:08 -0500

    Added .idea to .gitignore and added example JSDoc files

--- App.js
+++ App.js
@@ -2,6 +2,22 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
+/** @namespace */
+var exampleVar = {
+    /**
+     * Repeat <tt>str</tt> several times.
+     * @param {string} str The string to repeat.
+     * @param {number} [times=1] How many times to repeat the string.
+     * @returns {string}
+     */
+    repeat: function(str, times) {
+        if (times === undefined || times < 1) {
+            times = 1;
+        }
+        return new Array(times+1).join(str);
+    }
+};
+
 export default function App() {
   return (
     <View style={styles.container}>
