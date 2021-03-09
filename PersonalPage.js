9220971885aa7c55e6113f01af5933a50fc355c0
Aszliah Delicana <aszliahdelicana@gmail.com>
2021-03-09 17:14:32 -0500

    Prepare for moving detailedView to its own page rather than using Modals.

--- PersonalPage.js
+++ PersonalPage.js
@@ -5,8 +5,7 @@
 
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button, Pressable, TouchableOpacity } from 'react-native';
-import Modal from 'modal-enhanced-react-native-web';
+import { StyleSheet, Text, View, Image, Modal, Button, Pressable, TouchableOpacity } from 'react-native';
 
 /**
  * @class Contains function for rendering the personal page.
