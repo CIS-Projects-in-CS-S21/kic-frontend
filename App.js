e1c4d74bf46dfd2b36e480906509c0edf42aec7f
Priya Nigam <priya.nigam@temple.edu>
2021-03-13 08:38:27 -0500

    Added PersonalPageNav.js which contains stack navigation for personal page.

--- App.js
+++ App.js
@@ -7,17 +7,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LogIn from "./User-Authentication/LogIn";
-import SignUp from "./User-Authentication/SignUp";
-import PersonalPage from "./Personal-Page/PersonalPage";
-import MentalHealthLog from "./Mental-Health/MentalHealthLog";
-import FindHelp from "./Mental-Health/FindHelp";
 import UserFeed from './User-Feed/UserFeed';
 import KIC_Style from "./Components/Style";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Explore from "./Explore-Page/Explore";
 import Post from "./Post/Post";
+import PersonalPageNav from "./Personal-Page/PersonalPageNav";
 
 const Tab = createBottomTabNavigator();
 
@@ -25,7 +20,7 @@ function MyTabs() {
     return (
         <Tab.Navigator>
             <Tab.Screen name="Feed" component={UserFeed} />
-            <Tab.Screen name="Profile" component={PersonalPage} />
+            <Tab.Screen name="Profile" component={PersonalPageNav} />
             <Tab.Screen name="Post" component={Post} />
             <Tab.Screen name="Explore" component={Explore} />
         </Tab.Navigator>
