8797f9dc78a2ed3faef59bf8ba65cf7da50bfb0f
Aszliah Delicana <aszliahdelicana@gmail.com>
2021-03-15 13:32:43 -0400

    Fixed LogIn to require username instead of email

--- User-Authentication/LogIn.js
+++ User-Authentication/LogIn.js
@@ -23,7 +23,7 @@ export default function logIn() {
       "http://test.api.keeping-it-casual.com"
     );
     let req = new GetJWTTokenRequest();
-    req.getUsername(email);
+    req.getUsername(username);
     req.getPassword(password);
     client.getJWTToken(req, {}).then(res => {
       // On successful login, take user to user feed
@@ -38,7 +38,7 @@ export default function logIn() {
       <div className="form">
               <form onSubmit={handleSubmit}>
                 <div className="formInput">
-                  <input type="text" value={email} onChange={e => setEmail(e.target.value)} className="formDefault" placeholder="Email" required="required" />
+                  <input type="text" value={username} onChange={e => setUsername(e.target.value)} className="formDefault" placeholder="Username" required="required" />
                 </div>
                 <div className="formInput">
                   <input type="password" value={password} onChange={e => setPassword(e.target.value)} className="formDefault" placeholder="Password" required="required" />
