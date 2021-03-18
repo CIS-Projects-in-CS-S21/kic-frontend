4c130cf6cf9aa993b2f32a961c333b9bef77bd58
Aszliah Delicana <aszliahdelicana@gmail.com>
2021-03-18 16:59:36 -0400

    Starting to implement Async and trying to catch login error where app will users log in with any valid username

--- User-Authentication/LogIn.js
+++ User-Authentication/LogIn.js
@@ -5,10 +5,9 @@ import './SignUpStyle.css';
 import React from 'react';
 import { useNavigation } from '@react-navigation/native'; 
 import { useState} from 'react';
-import { Button } from 'react-native';
+import { AsyncStorage, Button } from 'react-native';
 import { UsersClient } from "../gen/proto/UsersServiceClientPb";
 import { GetJWTTokenRequest } from '../gen/proto/users_pb';
-
 
 export default function logIn() {
 
@@ -25,14 +24,40 @@ export default function logIn() {
     } else {
         url = "https://api.keeping-it-casual.com";
     }
+
     const client = new UsersClient(url);
+
     let req = new GetJWTTokenRequest();
     req.setUsername(username);
     req.setPassword(password);
+
     client.getJWTToken(req, {}).then(res => {
-      // On successful login, take user to user feed
+        // Store token in cookies
+        const storeToken = async () => {
+        await CookieManager.clearAll() //clearing cookies stored
+        //natively before each
+        //request
+        try {
+            await AsyncStorage.setItem(
+                '@MyToken:key',
+                res.getJWTToken()
+            );
+        } catch (error) {
+            // Error saving data
+        }
+    }
+
+      // Log response
       console.log(res)
-      navigation.navigate('TabNavigation')
+
+      // On successful login, take user to user feed
+
+      if (res.length > 0){
+            navigation.navigate('TabNavigation')
+      }
+      else{
+            console.log("No token received")
+      }
     }).catch(e => {
           console.log(e);
     });
