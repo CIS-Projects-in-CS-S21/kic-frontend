230f1f90d9ce6aeda3e07166b9a18558cc34bdbe
Priya Nigam <priya.nigam@temple.edu>
2021-03-21 10:12:02 -0400

    Created post nav js file to deal with post page navigation

---
+++ Post/PostNav.js
@@ -0,0 +1,48 @@
+/**
+ * @fileoverview Generates navigation for posting, which involves screens to choose from camera roll, create a caption, and share
+ */
+
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import {createStackNavigator} from "@react-navigation/stack";
+import MentalHealthLog from "../Mental-Health/MentalHealthLog";
+import FindHelp from "../Mental-Health/FindHelp";
+import PersonalPage from "./PersonalPage";
+import Post from "./Post";
+
+const Stack = createStackNavigator();
+
+/**
+ * @class Contains function for rendering the personal page navigation.
+ */
+class PostNav extends React.Component {
+    /**
+     * Renders personal page navigation components.
+     * @returns {Component}
+     */
+    render() {
+        return (
+            <Stack.Navigator>
+                <Stack.Screen
+                    name="Post"
+                    component={Post}
+                />
+            </Stack.Navigator>
+        );
+    }
+}
+
+/**
+ * @constant styles creates stylesheet for post page components
+ */
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 20,
+    },
+});
+
+export default PostNav;
