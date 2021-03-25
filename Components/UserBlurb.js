2e4f220916a6f1b36ba6a194594783ffa925b090
Aszliah Delicana <aszliahdelicana@gmail.com>
2021-03-25 16:36:33 -0400

    Friends page baseline with link on personal page

--- Components/UserBlurb.js
+++ Components/UserBlurb.js
@@ -1,75 +1,103 @@
 /**
-* @fileoverview The UserBlurb component is a reusable component for a blurb of a user that
-* displays their username, display name, icon, and bio.
-*/
+ * @fileoverview A component that displays a user's name, username, and bio
+ */
 
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, Modal, Button, Pressable, TouchableOpacity } from 'react-native';
+import KIC_Style from "../Components/Style";
+import PostDetails from "../Components/PostDetails";
+import ProfileHeader from "../Components/ProfileHeader";
 
 /**
-* @class Contains function for rendering a UserBlurb.
-*/
+ * @class Contains function for rendering the detailed post view.
+ */
 class UserBlurb extends React.Component {
-    /*
-    * Class constructor
-    */
-    constructor(props) {
-        super();
-
-        this.state = {
-            username: '',
-            displayName: '',
-            bio: '',
-        };
-    }
-
   /**
-   * Renders personal page components.
-   * @returns {FriendsPage}
+   * Renders the UserBlurb components.
+   * @returns a {UserBlurb}
    */
   render() {
       return (
         <View style={styles.container}>
-            <Image style={styles.icon}>{icon}</Text>
-            <View style={styles.details}>
-                <View style={styles.names}>
-                    <Text style={styles.displayName}>{displayName}</Text>
-                    <Text style={styles.userName}>{username}</Text>
-                </View>
-                <Text style={styles.bio}>{bio}</Text>
-            </View>
+              {/* User's icon */}
+              <Image
+                style={styles.icon}
+                source = {require('../assets/default/default_icon_2.png')}
+              />
+
+              {/* User's blurb */}
+              <View style ={styles.userInfo}>
+                  {/* User's display name and handle */}
+                  <View style ={styles.userID}>
+                      {/* Display name */}
+                      <Text style ={styles.textDisplayName}>{this.props.userFirstName} {this.props.userLastName}</Text>
+
+                      {/* Handle */}
+                      <Text style ={styles.textHandle}>@{this.props.userHandle}</Text>
+                  </View>
+                  {/* # of posts and friends */}
+                  <Text style ={styles.textBio}>{this.props.userBio}</Text>
+              </View>
+
+          <StatusBar style="auto" />
         </View>
       );
   }
 }
 
 /**
- * @constant styles creates stylesheet for personal page components
+ * @constant styles creates stylesheet for an individual UserBlurb's components.
  */
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    justifyContent: 'flex-start',
-    flexDirection: 'row',
-    flex: 1,
-  },
-  details: {
-    justifyContent: 'flex-start',
-    alignItems: 'left',
-    flexDirection: 'column',
-    flex: 1,
-  },
-  names: {
-    flexDirection: 'row',
-    flex: 1,
-  },
-  bio: {
-    alignItems: 'left',
-  },
-  icon: {
-    justifyContent: 'flex-start',
-  },
+    container: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        backgroundColor: '#c3dee6',
+        paddingVertical: 20,
+        justifyContent: 'flex-start',
+        paddingTop: 30,
+        paddingBottom: 30,
+        paddingRight: 10,
+        paddingLeft: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 10,
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40,
+        borderBottomLeftRadius: 40,
+    },
+    icon: {
+        width: 70,
+        height: 70,
+        borderTopRightRadius: 35,
+        borderTopLeftRadius: 35,
+        borderBottomRightRadius: 35,
+        borderBottomLeftRadius: 35,
+        marginRight: 15,
+        marginLeft: 15,
+    },
+    userInfo: {
+        flexDirection: 'column',
+        width: '80%',
+        paddingRight: 10,
+        flex: 1,
+    },
+    userID: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+    },
+    textDisplayName: {
+        fontSize: 15,
+        marginRight: 5,
+    },
+    textHandle: {
+        fontSize: 15,
+        fontWeight: "bold",
+    },
+    textBio: {
+        fontSize: 12,
+    }
 });
 
 export default UserBlurb;
