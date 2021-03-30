2117862eb456ee56d35c8e24e1346341b2fd65e9
Priya Nigam <priya.nigam@temple.edu>
2021-03-30 09:36:24 -0400

    Added icons to tab navigator and altered tab navigator styling to make it more aesthetically pleasing

--- TabNavigation.js
+++ TabNavigation.js
@@ -10,13 +10,42 @@ import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Explore from "./Explore-Page/Explore";
 import PersonalPageNav from "./Personal-Page/PersonalPageNav";
 import PostNav from "./Post/PostNav";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Feed" component={UserFeed} />
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
+
+                    if (route.name === 'Feed') {
+                        iconName = 'home-outline';
+                        color = '#ffff';
+                    } else if (route.name === 'Profile') {
+                        iconName =  'person-circle-outline' ;
+                        color = '#ffff';
+                    } else if (route.name === 'Post') {
+                        iconName = 'add-circle-outline' ;
+                        color = '#ffff';
+                    } else if (route.name === 'Explore') {
+                        iconName =  'search-outline';
+                        color = '#ffff';
+                    }
+
+                    // You can return any component that you like here!
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+            })}
+            tabBarOptions={{
+                showLabel: false,
+                style: { backgroundColor: '#b3d2db', paddingBottom: 3 },
+
+            }}
+        >
+            <Tab.Screen name="Feed" component={UserFeed}/>
             <Tab.Screen name="Profile" component={PersonalPageNav} />
             <Tab.Screen name="Post" component={PostNav} />
             <Tab.Screen name="Explore" component={Explore} />
