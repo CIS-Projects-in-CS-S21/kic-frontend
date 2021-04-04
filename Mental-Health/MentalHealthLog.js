484024a630b6a33e24c737b6b963733b9898696e
Priya Nigam <priya.nigam@temple.edu>
2021-04-04 18:36:41 -0400

    Created mental health tracker UI with likert scale and updated styling of buttons

--- Mental-Health/MentalHealthLog.js
+++ Mental-Health/MentalHealthLog.js
@@ -3,49 +3,81 @@
  */
 
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Image, Button, TouchableOpacity, SafeAreaView} from 'react-native';
 import KIC_Style from "../Components/Style";
+import {useNavigation} from "@react-navigation/native";
+import Likert from 'react-likert-scale';
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 /**
  * @class Contains function for rendering MentalHealthLog screen.
  */
-class MentalHealthLog extends React.Component {
+export default function MentalHealthLog({ navigation }) {
   /**
    * Renders MentalHealth screen components.
    * @returns {Component}
    */
-  render() {
-      return (
-        <View style={KIC_Style.container}>
-          <Image
-            style={{width: 180, height: 180, resizeMode: 'contain'}}
-            source = {require('../assets/kic.png')}
-          />
-          <Text>Keeping It Casual Mental Health Page!</Text>
-          <Button
-            title = "Find Help!"
+  const [date, setDate] = useState(new Date());
+  const likertOptions = {
+        question: "What is your opinion of the President’s performance?",
+        responses: [
+            { value: 1, text: "Abysmal" },
+            { value: 2, text: "Poor" },
+            { value: 3, text: "Average", checked: true },
+            { value: 4, text: "Good" },
+            { value: 5, text: "Excellent" }
+        ],
+        onChange: val => {
+            console.log(val);
+        }
+  };
+  return (
+    <SafeAreaView style={KIC_Style.container}>
+      <Image
+        style={{width: 180, height: 180, resizeMode: 'contain', alignSelf: 'center'}}
+        source = {require('../assets/kic.png')}
+      />
+      <Text style = {KIC_Style.title}> Mental Health Tracker</Text>
+      <Text> Indicate Date and Time of Entry </Text>
+      {/*<DateTimePicker*/}
+      {/*   date={date}*/}
+      {/*   onDateChange={setDate}*/}
+      {/*/>*/}
+      <Text>
+          Rate your mental health on a scale from -5 (extremely depressed) to 5 (extremely anxious). 0 is neither anxious nor depressed.
+      </Text>
+      <Likert {...likertOptions} />
+      <Text>  Write a journal entry expanding on your mood. How are you feeling? Why do you think you're feeling this way? </Text>
+      <TouchableOpacity
+            style={KIC_Style.button}
+            onPress ={() =>
+                alert("Entry stored!")
+            }>
+            <Text style={KIC_Style.button_font}> Locate A Professional </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+            style={KIC_Style.button}
             onPress = {() =>
-                this.props.navigation.navigate('FindHelp')
-            }
-          />
-            <Button
-                title = "View History!"
-                onPress = {() =>
-                    this.props.navigation.navigate('MoodHistory')
-                }
-            />
+                navigation.navigate('FindHelp')}>
+            <Text style={KIC_Style.button_font}> Locate A Professional </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+            style={KIC_Style.button}
+            onPress = {() =>
+                navigation.navigate('MoodHistory')}>
+            <Text style={KIC_Style.button_font}> View Your Mood History </Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+            style={KIC_Style.button}
+            onPress = {() =>
+                navigation.navigate('Profile')}>
+            <Text style={KIC_Style.button_font}> Back to Personal Page </Text>
+      </TouchableOpacity>
+      <StatusBar style="auto" />
+    </SafeAreaView>
+  );
 
-          <Button
-            title = "Return to Personal Page!"
-            onPress = {() =>
-                this.props.navigation.navigate('Profile')
-            }
-          />
-          <StatusBar style="auto" />
-        </View>
-      );
-  }
 }
 
 /**
@@ -60,4 +92,3 @@ const styles = StyleSheet.create({
     },
 });
 
-export default MentalHealthLog;
