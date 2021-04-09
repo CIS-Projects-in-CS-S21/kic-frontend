044f974b420a6bc4786b964f2689eab7d3d7387c
Ryan McGregor <tug81211@temple.edu>
2021-04-09 08:59:54 -0400

    Changed the data uploaded from URI to base64

--- Post/PostInfo.js
+++ Post/PostInfo.js
@@ -125,7 +125,7 @@ export default function PostInfo(props) {
         file.setDatestored(date);
 
         //convert uri to int 8 Array which is needed for setting File
-        let your_bytes = Buffer.from(uri, "base64");
+        let your_bytes = Buffer.from(base64, "base64");
         req.setFile(Uint8Array.from(your_bytes));
         req.setFileinfo(file);
 
