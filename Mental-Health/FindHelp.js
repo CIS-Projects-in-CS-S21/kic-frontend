e127ab89a8bc3d88e7e63dea8b77ead4131cd394
Priya Nigam <priya.nigam@temple.edu>
2021-04-13 00:32:10 -0400

    Added search bar to mock find help page

--- Mental-Health/FindHelp.js
+++ Mental-Health/FindHelp.js
@@ -6,8 +6,9 @@ import { StatusBar } from 'expo-status-bar';
 import FeedHeader from '../Components/FeedHeader';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import {StyleSheet, Text, View, Image, Button, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, Image, Button, TouchableOpacity, TextInput, ScrollView} from 'react-native';
 import KIC_Style from '../Components/Style';
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 /**
  * @class Contains function for rendering FindHelp screen.
@@ -17,23 +18,54 @@ class FindHelp extends React.Component {
    * Renders FindHelp screen components.
    * @returns {Component}
    */
+  constructor(props) {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      searchString: '',
+      finishedLoading: false,
+    };
+
+    this.handleSearch = this.handleSearch(this)
+
+  }
+  setSearchString = (text) => {
+    this.setState({ searchString: text })
+  }
+
+  handleSearch() {
+    console.log("Searching for " + this.state.searchString + "...");
+  }
   render() {
     return (
       <SafeAreaView style={KIC_Style.outContainer}>
         <FeedHeader navigation={this.props.navigation} />
         <SafeAreaView style={KIC_Style.innerContainer}>
-          <Image
-            style={{ width: "100%", height: "80%", alignItems: "center", resizeMode: 'contain' }}
-            source={require('../assets/googlemap_sample.png')}
-          />
-          <Text>Keeping It Casual Find Help Page!</Text>
-          <TouchableOpacity
-              style={KIC_Style.button}
-              onPress={() =>
-                  this.props.navigation.navigate('MentalHealthLog')}>
-            <Text style={KIC_Style.button_font}> Return to Mental Health Log </Text>
-          </TouchableOpacity>
-          <StatusBar style="auto" />
+            <View style={{flexDirection: 'row', marginTop: 35, justifyContent: 'center' }}>
+              <TextInput
+                  style={KIC_Style.searchInput}
+                  textAlign = {'center'}
+                  onChange={(e) => this.setSearchString(e.nativeEvent.text)}
+                  placeholder="Search using a zipcode . . ."
+              />
+              <TouchableOpacity
+                  style={{ backgroundColor: '#b3d2db', borderRadius: 10, height: 30, justifyContent: 'center' }}
+                  onPress = {this.handleSearch}>
+                <Ionicons name="search-circle-outline" color='#ffff' size={25} />
+              </TouchableOpacity>
+            </View>
+            <Image
+              style={{ width: "100%", height: "80%", alignItems: "center", resizeMode: 'contain' }}
+              source={require('../assets/googlemap_sample.png')}
+            />
+            <TouchableOpacity
+                style={KIC_Style.button}
+                onPress={() =>
+                    this.props.navigation.navigate('MentalHealthLog')}>
+              <Text style={KIC_Style.button_font}> Return to Mental Health Log </Text>
+            </TouchableOpacity>
+            <StatusBar style="auto" />
         </SafeAreaView>
       </SafeAreaView>
     );
