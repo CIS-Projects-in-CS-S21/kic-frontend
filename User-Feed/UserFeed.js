8f2eb45cee3d24068521d9bc7e14d2acaeba5643
jaimer7 <jaimeriley@temple.edu>
2021-04-12 22:04:53 -0400

    Create user feed using stream

--- User-Feed/UserFeed.js
+++ User-Feed/UserFeed.js
@@ -4,11 +4,18 @@
  */
 
 import React from 'react';
-import { Platform, StyleSheet, ScrollView, StatusBar } from 'react-native';
+import { FlatList, Platform, StyleSheet, ScrollView, StatusBar, View , Text} from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FeedHeader from '../Components/FeedHeader';
 import FeedPost from '../Components/FeedPost';
 import KIC_Style from '../Components/Style';
+import { GetUserByIDRequest, GetUserByUsernameRequest, UpdateUserInfoRequest } from '../gen/proto/users_pb';
+import { GetFilesByMetadataRequest } from "../gen/proto/media_pb";
+import { GetFriendsForUserRequest } from "../gen/proto/friends_pb";
+import { GenerateFeedForUserRequest } from "../gen/proto/feed_pb"
+import TokenManager from "../Managers/TokenManager";
+import ClientManager from "../Managers/ClientManager";
+import UserManager from '../Managers/UserManager';
 
 
 /**
@@ -23,9 +30,93 @@ class UserFeed extends React.Component {
 
     // Define the initial state:
     this.state = {
-
+      myUser: null,
+      myUserid: '',
+      authString: '',
+      feedFiles: []
     };
   }
+
+  async componentDidMount() {
+    this.fetchUserInfo().then(response => {
+      console.log("Mounted userfeed success");
+    }).catch(error => {
+      console.log(error)
+    });
+  }
+
+  // Starts the process of fetching active user info.
+  fetchUserInfo() {
+    return this.callGetAuthString();
+  }
+
+  // Uses UserManager to fetch authstring then calls callgetUserID
+  callGetAuthString() {
+    let um = new UserManager();
+    return um.getAuthString().then(authString => { this.callGetUserID(um, authString) });
+  }
+
+  // Sets the authstring to the state and calls getMyUserID
+  callGetUserID(um, authString) {
+    this.setState({
+      authString: authString,
+    })
+    return um.getMyUserID().then(userID => { this.callGetUserByUserID(userID) });
+  }
+
+  // Inits GetUserByIDRequest and sends it through client
+  callGetUserByUserID(userID) {
+    let cm = new ClientManager();
+    let client = cm.createUsersClient();
+
+    let req = new GetUserByIDRequest();
+    req.setUserid(userID);
+    return client.getUserByID(req, { 'Authorization': this.state.authString }).then(res => { this.setUserInfo(cm, res, userID) })
+  }
+
+  // Sets the active userID to the state and then inits a GetFilesByMetadataRequest to retrieve user's own posts
+  setUserInfo(cm, res, userID) {
+    let user = res.getUser();
+    //let myusername = user.getUsername();
+    this.setState({
+      myUserid: userID,
+      myUser: user,
+    })
+
+    // Create a new request that will create a stream for files for ACTIVE USERS userid
+    let req = new GenerateFeedForUserRequest();
+    req.setUserid(this.state.myUserid);
+
+    let client = cm.createFeedClient();
+
+    let stream = client.generateFeedForUser(req, { 'Authorization': this.state.authString });
+    //Take stream of files, store them to the file posts array
+    stream.on('data', function (response) {
+      //console.log(response);
+      //console.log(response.getFileinfo()); 
+
+      let file = response.getFileinfo(); 
+      console.log("Files: " + file);
+      let files = this.state.feedFiles; 
+      let combinedfiles = files.concat(file);
+      
+      this.setState({
+        feedFiles : combinedfiles
+      })
+
+    }.bind(this));
+
+    //Prints and handles errors
+    stream.on('status', function (status) {
+
+    });
+
+    //When stream is over
+    stream.on('end', function (end) {
+
+    });
+  }
+
 
   /**
    * Renders user feed components.
@@ -38,14 +129,19 @@ class UserFeed extends React.Component {
         <FeedHeader navigation={this.props.navigation} />
         <SafeAreaView style={styles.container}>
           <ScrollView>
-            {/*Header containing logo and name*/}
-            {/*Posts, eventually will need to become a stream injected into individual FeedPosts*/}
-            <FeedPost
-              style={styles.feedPost} />
-            <FeedPost
-              style={styles.feedPost} />
-            <FeedPost
-              style={styles.feedPost} />
+            {/* FlatList that renders a UserBlurb per user in the friend list */}
+            <FlatList
+              style={styles.listcontainer}
+              data={this.state.feedFiles}
+              renderItem={({ item }) => <FeedPost
+                navigation={this.props.navigation}
+                authString={this.state.authString}
+                myUserid={this.props.myUserid}
+                file={item}
+              />}
+              keyExtractor={friend => friend.userid}
+            />
+            <Text style={KIC_Style.titlePost}>End of feed!</Text>
             <StatusBar style="auto" />
           </ScrollView>
         </SafeAreaView>
@@ -61,15 +157,15 @@ const styles = StyleSheet.create({
   container: {
     ...Platform.select({
       ios: {
-        top:30,
-        marginBottom:30,
+        top: 30,
+        marginBottom: 30,
       },
       android: {
-        top:30,
-        marginBottom:30,
+        top: 30,
+        marginBottom: 30,
       },
       default: {
-        top:60,
+        top: 60,
         marginBottom: 60,
       }
     }),
